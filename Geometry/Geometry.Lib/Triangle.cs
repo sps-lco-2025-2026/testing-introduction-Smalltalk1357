e38ddf9875094802540670ffa5fb783cabc08d30
Smalltalk1357 <alex.wan@icloud.com>
2026-01-20 12:58:39 +0000

    Updated TriangleTests.cs

--- Geometry/Geometry.Lib/Triangle.cs
+++ Geometry/Geometry.Lib/Triangle.cs
@@ -1,73 +1,52 @@
 namespace Geometry.Lib;
 
-public class Triangle
+public class Triangle(string name, double angleA, double angleB, double angleC, double lengthA, double lengthB, double lengthC)
 {
-    private int _angleA;
-    private int _angleB;
-    private int _angleC;
-    private int _lengthA;
-    private int _lengthB;
-    private int _lengthC;
-    private string _name;
-
-    public Triangle()
-    {
-        // Angle A is opposite length A e.t.c.
-        
-        _name = "Triangle";
-        _angleA = 60;
-        _angleB = 60;
-        _angleC = 60;
-        _lengthA = 10;
-        _lengthB = 10;
-        _lengthC = 10;
-    }
+    // Angle A is opposite length A e.t.c.
 
     private bool IsValid()
     {
-        int intAngles = _angleA + _angleB + _angleC;
-        if (intAngles > 180 || intAngles < 0) return false;
+        double intAngles = angleA + angleB + angleC;
+        if (intAngles is > 180 or < 0) return false;
         
-        if (_lengthA <= 0 || _lengthB <= 0 || _lengthC <= 0) return false;
+        if (lengthA <= 0 || lengthB <= 0 || lengthC <= 0) return false;
         
-        double sineRuleA = Math.Sin(_angleA) / _lengthA;
-        double sineRuleB = Math.Sin(_angleB) / _lengthB;
-        double sineRuleC = Math.Sin(_angleC) / _lengthC;
+        double sineRuleA = Math.Sin(angleA) / lengthA;
+        double sineRuleB = Math.Sin(angleB) / lengthB;
+        double sineRuleC = Math.Sin(angleC) / lengthC;
 
-        if (sineRuleA != sineRuleB || sineRuleB != sineRuleC) return false;
-        
-        return true;
+        return !(Math.Abs(sineRuleA - sineRuleB) > 0.001) && !(Math.Abs(sineRuleB - sineRuleC) > 0.001);
     }
 
     public bool IsRightAngled()
     {
         if (!IsValid()) return false;
-        return _angleA == 90 || _angleB == 90 || _angleC == 90;
+        return Math.Abs(angleA - 90) < 0.001 || Math.Abs(angleB - 90) < 0.001 || Math.Abs(angleC - 90) < 0.001;
     }
 
     public bool IsEquilateral()
     {
         if (!IsValid()) return false;
-        return _angleA == _angleB && _angleB == _angleC;
+        return Math.Abs(angleA - angleB) < 0.001 && Math.Abs(angleB - angleC) < 0.001;
     }
 
     public bool IsIsosceles()
     {
         if (!IsValid()) return false;
-        if (_angleA == _angleB || _lengthA == _lengthB) return true;
-        if (_angleB == _angleC || _lengthB == _lengthC) return true;
-        if (_angleA == _angleC || _lengthA == _lengthC) return true;
+        if (Math.Abs(angleA - angleB) < 0.001 || Math.Abs(lengthA - lengthB) < 0.001) return true;
+        if (Math.Abs(angleB - angleC) < 0.001 || Math.Abs(lengthB - lengthC) < 0.001) return true;
+        if (Math.Abs(angleA - angleC) < 0.001 || Math.Abs(lengthA - lengthC) < 0.001) return true;
         return false;
     }
 
     public double Area()
     {
-        if (!IsValid()) return 0;
-        return 0.5 * _lengthA * _lengthB * Math.Sin(_angleC);
+        // if (!IsValid()) return 0;
+        return 0.5 * lengthA * lengthB * Math.Sin(angleC);
     }
     
     public override string ToString()
     {
-        return _name;
+        return name;
     }
 }
--- Geometry/Geometry.Tests/TriangleTests.cs
+++ Geometry/Geometry.Tests/TriangleTests.cs
@@ -8,6 +8,7 @@ public class TriangleTests
     [TestMethod]
     public void TestOnce()
     {
-        
+        Triangle tri = new("Triangle", 30, 75, 75, 10, 19.31851653, 19.31851653);
+        Assert.AreEqual(93.30127019, tri.Area());
     }
 }
